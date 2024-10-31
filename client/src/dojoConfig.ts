c23fe44aa4923151de2ad5330af268d4ce2d4ac8
0xmugen <pierre.semanne@gmail.com>
2024-10-31 23:24:52 +0100

    feat: fixed middle end

--- client/src/dojoConfig.ts
+++ client/src/dojoConfig.ts
@@ -3,9 +3,11 @@ import manifest from '../../contracts/manifests/dev/deployment/manifest.json'
 import { createDojoConfig } from '@dojoengine/core'
 
 export const dojoConfig = createDojoConfig({
-  toriiUrl: 'https://api.cartridge.gg/x/skeleton-smash/torii',
-  rpcUrl: 'https://api.cartridge.gg/x/skeleton-smash/katana',
+  toriiUrl: 'http://127.0.0.1:8080',
+  rpcUrl: 'http://127.0.0.1:5050',
   manifest,
+  masterAddress: '0x541da8f7f3ab8247329d22b3987d1ffb181bc8dc7f9611a6eccec3b0749a585',
+  masterPrivateKey: '0x736adbbcdac7cc600f89051db1abbc16b9996b46f6b58a9752a11c1028a8ec8',
 })
 
 export type Config = typeof dojoConfig
