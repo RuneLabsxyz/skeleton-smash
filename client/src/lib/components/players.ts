55bbd82f2d95dde134fe667c67bff20b248cd813
0xmugen <pierre.semanne@gmail.com>
2024-11-01 08:01:04 +0100

    feat: added enemy characters

---
+++ client/src/lib/components/players.ts
@@ -0,0 +1,3 @@
+import { writable } from "svelte/store";
+
+export let playerPosition = writable(15);
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-    import Grid from "$lib/components/Grid.svelte";
+    import Grid from "$lib/components/Grid.svelte";    
 </script>
 
 <div class="w-screen flex h-screen justify-center items-center flex-col">
-    <h1 class="font-bold text-3xl mb-5">Skeleton Bash</h1>
     <Grid></Grid>
 </div>
