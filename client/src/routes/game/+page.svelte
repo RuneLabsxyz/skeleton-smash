d036fc6c8bea886dcd2c3f16a70a784e3dd9d658
0xmugen <pierre.semanne@gmail.com>
2024-11-01 23:54:59 +0100

    feat: navigate to game if it exists

--- client/src/routes/game/+page.svelte
+++ client/src/routes/game/+page.svelte
@@ -1,9 +1,18 @@
-<script>
+<script lang="ts">
     import { currentPlayerRun, startRun } from "$lib/api/run";
     import { currentPlayerRoom } from "$lib/api/room";
 
 
-    currentPlayerRoom.subscribe((e) => console.log("room: ", e));
+    let roomId: Number | null = $state(null);
+
+    currentPlayerRoom.subscribe((e) => {
+        roomId = e?.room_id ?? null;
+    });
+
+    $effect(() => {
+        if (roomId == null) return;
+        window.location.href = `/game/${roomId}`;
+    })
 
     async function startGame() {
         await startRun();
