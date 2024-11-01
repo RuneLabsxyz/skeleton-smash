fdc91b2ff573a1cd91a789fee7bee0a105b65803
Valentin Ricard <me@valentin.red>
2024-11-01 21:57:55 +0000

    feat: Added all base models

--- contracts/scripts/spawn.sh
+++ contracts/scripts/spawn.sh
@@ -7,4 +7,4 @@ export RPC_URL="http://localhost:5050";
 export WORLD_ADDRESS=$(cat ./manifests/dev/deployment/manifest.json | jq -r '.world.address')
 
 # sozo execute --world <WORLD_ADDRESS> <CONTRACT> <ENTRYPOINT>
-sozo execute --world $WORLD_ADDRESS skeleton_smash::systems::actions::IActions initialize --wait
+sozo execute --world $WORLD_ADDRESS actions initialize --wait
