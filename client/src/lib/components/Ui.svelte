b6dae5ee51cc9c989ce4b3c279a8e87dac80b793
0xmugen <pierre.semanne@gmail.com>
2024-11-03 17:36:06 +0100

    feat: display level

--- client/src/lib/components/Ui.svelte
+++ client/src/lib/components/Ui.svelte
@@ -3,14 +3,16 @@
     import { currentPlayerRun } from "$lib/api/run";
     import { type Run } from "$src/dojo/models.gen";
     import Background from "./ui/Background.svelte";
-
+    import Level from "./ui/Level.svelte";
     let { run } = $props<{
         run: Run | null;
     }>();
+
 </script>
 
 <div class="absolute top-0 left-0 w-full h-full flex items-center">
     {#if run?.move_count == 0}
         <StartBanner />
     {/if}
+    <Level level={run.level} />
 </div>
---
+++ client/src/lib/components/ui/Level.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    let { level } = $props<{ level: number }>();
+</script>
+
+
+<div class="absolute top-4 left-4 text-xl font-bold">Level {level}</div>
