ef1edbdbf70844ddff0106f3a32e45224e4b2859
Valentin Ricard <me@valentin.red>
2024-11-04 06:53:22 +0000

    chore: Improved tutorial a bit

--- client/src/lib/components/ui/startBanner.svelte
+++ client/src/lib/components/ui/startBanner.svelte
@@ -1,5 +1,5 @@
 <div class="w-full h-1/4 bg-red-800/50 flex items-center justify-center">
     <div class="container mx-auto px-4 text-center text-white text-4xl">
-        Use the arrow keys to move
+        Swipe the screen to move
     </div>
 </div>
