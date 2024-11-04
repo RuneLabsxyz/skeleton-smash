4f537e615109d6922e2ddff76404ab004e89a940
0xmugen <pierre.semanne@gmail.com>
2024-11-04 03:06:20 +0100

    feat: start page

--- client/src/routes/game/+page.svelte
+++ client/src/routes/game/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { startRun } from "$lib/api/run";
     import { currentPlayerRoom } from "$lib/api/room";
+    import Background from "$lib/components/ui/Background.svelte";
 
     let roomId = $state<Number | null>(null);
 
@@ -18,4 +19,15 @@
     }
 </script>
 
-<button onclick={startGame}>Start Game</button>
+<div class="w-screen h-screen flex justify-center items-center z-20 relative">
+    <button onclick={startGame} class="text-xl font-bold px-8 py-4 bg-[#4F6660] text-white border-2 border-black hover:bg-[#3D514C]">Start Game</button>
+</div>
+
+<div class="absolute top-0 left-1/3 w-1/3 h-screen grid grid-cols-5 grid-rows-[repeat(20,1fr)] gap-0 z-10">
+    {#each Array(100) as _}
+        <div class="w-full h-full bg-center bg-cover" style="background-image: url('/bg/wall.jpg')"></div>
+    {/each}
+</div>
+
+
+<Background />
