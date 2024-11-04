fe3bcb9ce27e52ee7fd4f85c4bb34f25217200f9
Valentin Ricard <me@valentin.red>
2024-11-04 07:07:12 +0000

    Change the text on desktop

--- client/src/lib/components/ui/startBanner.svelte
+++ client/src/lib/components/ui/startBanner.svelte
@@ -1,5 +1,9 @@
 <div class="w-full h-1/4 bg-red-800/50 flex items-center justify-center">
     <div class="container mx-auto px-4 text-center text-white text-4xl">
-        Swipe the screen to move
+        {#if window.matchMedia("(pointer: coarse)").matches}
+            Swipe the screen to move
+        {:else}
+            Use the arrow keys to move
+        {/if}
     </div>
 </div>
